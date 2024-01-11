89167bffff22f0e583a39110c922e6b6cd883941
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-01-11 15:21:37 +0900

    Создан компонент "Counter"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Counter from "./components/Counter";
 
 function App() {
+
+
   return (
     <div className="App">
-      
+       <Counter/>
+       <Counter/>
+       <Counter/>
+       <Counter/>
     </div>
   );
 }
