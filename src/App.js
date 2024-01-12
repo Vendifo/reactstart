2d28d064de2d41cd4ff1db3ab7779ee35c522ea4
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-01-13 08:17:09 +0900

    Сортировка постов

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostList from "./components/PostList";
 import MyInput from "./components/UI/input/MyInput"
 import MyButton from "./components/UI/button/MyButton";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
 
         ])
 
+        const [selectedSort, setSelectedSort] = useState('')
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
     }
@@ -22,12 +25,37 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter (p => p.id !== post.id))
     }
+
+    const sortPosts = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a , b) =>a[sort].localeCompare(b[sort])))
+    }
         
 
     return (
         <div className="App">
             <PostForm create={createPost} />
-            <PostList remove={removePost} posts={posts} title='Посты про JS'/>
+            <hr style={{margin: '15px 0'}} ></hr>
+            <div>
+                <MySelect
+                    value={selectedSort}
+                    onChange={sortPosts}
+                    defaultValue="Сортировка"
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'},
+                    ]}
+                />
+            </div>
+            {posts.length 
+                ?
+                <PostList remove={removePost} posts={posts} title='Посты про JS'/>
+                :
+                <h1 style={{textAlign: 'center'}} >
+                    Посты не найдены!
+                </h1>
+            }
+            
         </div>
     );
 }
